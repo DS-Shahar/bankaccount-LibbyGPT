e7b88b88bbc99b909e7037af1cd324dde12c0612
LibbyGPT <libibomiller@gmail.com>
2024-09-15 18:07:37 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,46 @@
 package BankAccountPackage;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount BobAccount = new BankAccount(1000, -1000);
-        BankAccount LisaAccount = new BankAccount(1000, -1000);
+    	Scanner scanner = new Scanner(System.in);
+    	
+        BankAccount bobAccount = new BankAccount(1000, -1000, "Bob");
+        BankAccount lisaAccount = new BankAccount(1000, -1000, "Lisa");
 
         System.out.println("Initial Balances:");
-        System.out.println("Bob's balance: " + BobAccount.getBalance());
-        System.out.println("Lisa's balance: " + LisaAccount.getBalance());
+        System.out.println(bobAccount);
+        System.out.println(lisaAccount);
 
-        boolean successTransfer = BobAccount.transfer(500, LisaAccount);
+        boolean successTransfer = bobAccount.transfer(500, lisaAccount);
         System.out.println("\nTransfer of 500 from Bob to Lisa: " + (successTransfer ? "Successful" : "Failed"));
-        System.out.println("Bob's balance: " + BobAccount.getBalance());
-        System.out.println("Lisa's balance: " + LisaAccount.getBalance());
+        System.out.println(bobAccount);
+        System.out.println(lisaAccount);
 
-        successTransfer = BobAccount.transfer(1600, LisaAccount);
+        successTransfer = bobAccount.transfer(1600, lisaAccount);
         System.out.println("\nTransfer of 1600 from Bob to Lisa: " + (successTransfer ? "Successful" : "Failed"));
-        System.out.println("Bob's balance: " + BobAccount.getBalance());
-        System.out.println("Lisa's balance: " + LisaAccount.getBalance());
+        System.out.println(bobAccount);
+        System.out.println(lisaAccount);
+        
+        System.out.print("Enter the length of the rectangle: ");
+        int length = scanner.nextInt();
+        
+        System.out.print("Enter the width of the rectangle: ");
+        int width = scanner.nextInt();
+        
+        Rectangle rect = new Rectangle(length, width);
+        
+        rect.draw();
+        
+        System.out.println("Area: " + rect.calcArea());
+        System.out.println("Perimeter: " + rect.calcPerimeter());
+        System.out.print("Enter the scale factor: ");
+        int scaleFactor = scanner.nextInt();
+        
+        rect.scale(scaleFactor);
+        System.out.println("Scaled Rectangle: " + rect);
+        
+        rect.draw();
     }
 }
